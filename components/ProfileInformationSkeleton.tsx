83df74fb837e159b0e95c68769ea76e39be289fa
Juan Vivas <juanvivas043@gmail.com>
2025-09-27 12:27:30 -0400

    traduccion generos profile page

--- components/ProfileInformationSkeleton.tsx
+++ components/ProfileInformationSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function ProfileInformationSkeleton() {
   return (
-    <div className="max-w-4xl mx-auto p-4 md:p-0"> {/* Añadido padding para dispositivos móviles */}
+    <div className="min-h-screen max-w-4xl mx-auto p-4 md:p-0"> {/* Añadido padding para dispositivos móviles */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Columna principal izquierda */}
         <div className="lg:col-span-2">
