76beab0c33dd90e3921c9803d7fa968aaf68195b
Juan Vivas <juanvivas043@gmail.com>
2025-11-06 11:40:13 -0400

    texto del home para usuarios y no usuarios

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+"use client"
+
+import { useAuth } from "@/context/auth-context";
+
 export default function Home() {
+    const {user} = useAuth()
+
     return (
         <div 
             style={{
@@ -14,17 +20,34 @@ export default function Home() {
                 before:bg-black/75 
             "
         >
-            <div className="flex flex-col max-w-150 z-10 text-left px-10 py-10">
-                <span className="text-xl font-bold bg-gradient-to-r from-pink-500 to-white text-transparent bg-clip-text mb-5">
-                    Matching
-                </span> 
-                <h1 className="text-4xl font-bold text-white mb-5">
-                    App de Citas
-                </h1>
-                <p className="text-lg text-gray-300 mb-5 ">
-                    Matching es una app de citas que permite a los usuarios encontrarse con personas compatibles basándose en sus preferencias y intereses. Es una app diseñada para practicar la conexión y el desarrollo web moderno.
-                </p>
-            </div>
+            {
+                user ? (
+                    <div className="flex flex-col max-w-145 z-10 text-justify px-10 py-10">
+                        <span className="text-xl font-bold bg-gradient-to-r from-pink-500 to-white text-transparent bg-clip-text mb-5">
+                            Matching
+                        </span>
+                        <h1 className="text-4xl font-bold text-white mb-5">
+                            Comienza a Conectar!
+                        </h1>
+                        <p className="text-lg text-gray-300 mb-5 ">
+                            Explora la App de Matching y encuentra personas compatibles basadas en tus preferencias y intereses. Comienza personalizando tu perfil para que empieces a conectarte con personas que compartan tus intereses.
+                        </p>
+                    </div>
+
+                ) : (
+                    <div className="flex flex-col max-w-145 z-10 text-justify px-10 py-10">
+                        <span className="text-xl font-bold bg-gradient-to-r from-pink-500 to-white text-transparent bg-clip-text mb-5">
+                            Matching
+                        </span>
+                        <h1 className="text-4xl font-bold text-white mb-5">
+                            App de Citas
+                        </h1>
+                        <p className="text-lg text-gray-300 mb-5 ">
+                            Matching es una app de citas que permite a los usuarios encontrarse con personas compatibles basándose en sus preferencias y intereses. Es una app diseñada para practicar la conexión y el desarrollo web moderno.
+                        </p>
+                    </div>
+                )
+            }
         </div>
     );
 }
